bf6418b0cb569f8e2ea8579b46e0e1b2c02bb5e6
vouwen <127990244+vouwen@users.noreply.github.com>
2023-09-26 23:51:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Vitor',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
